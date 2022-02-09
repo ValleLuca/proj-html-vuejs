b188e712a86db1b2ca613a69f263d7f3c9018b3e
ValleLuca <valle.lucaAb@outlook.it>
2022-02-09 17:32:16 +0100

    Conclusione

--- src/components/section/sectionFooter/FooterBottom.vue
+++ src/components/section/sectionFooter/FooterBottom.vue
@@ -1,13 +1,48 @@
 <template>
-  <div class="bgImage">
-
+  <div>
+    <div class="bgImage position-relative">
+      <div class="position-absolute top-50 start-50 translate-middle text-light text-center">
+        <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores ut natus aut, temporibus odio tenetur nostrum beatae. Quasi culpa tenetur porro esse vero? Quam cumque quos ducimus voluptatibus adipisci dolores, quidem ipsam dicta expedita voluptates sunt perferendis"</p>
+        <p>FRIEDA FRIEDSON, MASSEUSE</p>
+      </div>
+    </div>
+    <div class="backImg">
+      <div class="container padding">
+          <div class="row">
+              <div class="col-2 d-flex justify-content-center align-items-center">
+                <div class="optionBox text-center ">
+                  <div class="p-3">
+                    <div class="text-light fSizePerc fw-bold">25%</div>
+                    <div class="text-light sizeFont">SPECIAL OFFERT</div>                    
+                  </div>
+                </div>
+              </div>
+              <div class="col d-flex align-items-center">
+                <div>
+                  <p class="text-light sizeCFooter">Take some time. Treat yourself. You deserve it.</p>
+                  <p class="text-light">Book a treatment this month and receive a 25% on all further treatments</p>
+                </div>
+              </div>
+              <div class="col-2 d-flex justify-content-center align-items-center">
+                <div>
+                  <button class="text-light px-4 py-2 rounded-pill optionButton">BOOK NOW</button>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+    <footer-botoom-end-page />
   </div>
 </template>
 
 <script>
+import FooterBotoomEndPage from './footerBotoomSection/FooterBotoomEndPage.vue'
 
 export default {
   name: 'FooterBottom',
+  components: { 
+    FooterBotoomEndPage 
+  },
 
 }
 </script>
@@ -18,8 +53,37 @@ export default {
 
 .bgImage{
   background-image: url("../../../assets/img/Home-quota-new.jpg");
+  background-position: left center;
   min-height: 700px;
   width: auto;
 }
 
+.backImg{
+  background-image: url('../../../assets/img/ptb_cta-new.jpg');
+}
+
+.padding{
+  padding: 150px 0;
+}
+
+.fSizePerc{
+  font-size: 50px;
+}
+
+.sizeFont{
+  font-size: 11px;
+}
+
+.sizeCFooter{
+  font-size: 35px;
+}
+
+.optionButton{
+  background-color: #b8ad9f;
+  border: 1px solid #b8ad9f;
+}
+
+.optionBox{
+  border: 1px solid #b8ad9f;
+}
 </style>
---
+++ src/components/section/sectionFooter/footerBotoomSection/FooterBotoomEndPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="p-5">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col">
+                  <img src="../../../../assets/img/avada-spa-logo-new.png">
+              </div>
+              <div class="col-6">
+                  <div class="text-center">
+                      <span>@COPIRIGHT 2012 - 2020 | AWADA THEMEFUSION | ALL RIGHTS RESERVED </span>
+                      <p>POWERED BY LUCA VALLE</p>
+                  </div>
+              </div>
+              <div class="col">
+                  <div class="d-flex justify-content-evenly">
+                        <div class="optionBoxSocial">
+                            <i class="fab fa-facebook-f"></i>
+                        </div>
+                        <div class="optionBoxSocial">
+                            <i class="fab fa-twitter"></i>
+                        </div>
+                        <div class="optionBoxSocial">
+                            <i class="fab fa-instagram"></i>
+                        </div>
+                        <div class="optionBoxSocial">
+                            <i class="fab fa-youtube"></i>
+                        </div>
+                  </div>
+                  
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "FooterBotoomEndPage",
+}
+</script>
+
+<style scoped lang="scss">
+
+.optionBoxSocial{
+    background-color: #e7e7e7;
+    padding: 10px 15px;
+    color: #bdbcbc;
+    &:hover{
+        color: black;
+        cursor: pointer;
+    }
+}
+
+</style>
